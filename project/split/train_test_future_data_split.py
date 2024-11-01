5bd134b5baffffb39775a2634124d8b79e196e67
kdunkerl <kdunkerl@andrew.cmu.edu>
2024-11-01 09:08:55 -0400

    fix smote

--- project/split/train_test_future_data_split.py
+++ project/split/train_test_future_data_split.py
@@ -1,10 +1,13 @@
 import sklearn as sk
 import pandas as pd
-import os
+from imblearn.over_sampling import SMOTE
+import json
 
-# train test split, cross validation with time series (date_posted) ?
-# just a set up to get started, will update once OHE done
-from sklearn.model_selection import train_test_split
+def load_config(config_file="../config.json"):
+    """Load configuration from a JSON file."""
+    with open(config_file, 'r') as file:
+        config = json.load(file)
+    return config
 
 def split_data(df):
     '''
@@ -22,7 +25,31 @@ def split_data(df):
     # also there may be projects that didn't finish their funding rounds (-1) dropping the unknowns
     filtered_train_test_df = train_test_df[train_test_df['fully_funded'] != -1]
 
-    return filtered_train_test_df, future_df
+    return filtered_train_test_df, future_df 
+
+def smote_balancing(df, pov_col_name, config):
+    '''
+    Use SMOTE to resample/upsample data to balance the two classes
+    model_type: string name
+    X_train: x training data (pandas dataframe)
+    y_train: y training data (pandas dataframe)
+    X_test: x testing data (pandas dataframe)
+    '''
+    smote = SMOTE(random_state=42)
+
+    X = df.drop(['fully_funded', 'projectid', 'date_posted'], axis=1)
+    y = df['fully_funded']
+
+    new_X = X[X[pov_col_name] == True].drop(columns=[
+        *config["poverty_columns"].values()], axis=1)
+
+    new_y = y[X[pov_col_name] == True]
+
+    X_res, y_res = smote.fit_resample(new_X, new_y)
+    print(f'SMOTE upsampled from {len(new_y)} to {len(y_res)} samples')
+
+    X_res["fully_funded"] = y_res
+    return X_res
 
 if __name__ == "__main__":
     cleaned_dataset_path = "../outputs/cleaned_dataset.csv"
@@ -35,5 +62,12 @@ if __name__ == "__main__":
     
     train_test_df.to_csv(train_test_path, index=False)
     future_df.to_csv(future_path, index=False)
+
+    config = load_config()
+
+    for pov_lvl, pov_col_name in config["poverty_columns"].items():
+        smote_dataset = smote_balancing(df, pov_col_name, config)
+        smote_path = f"../outputs/{pov_lvl}_pov_lvl_train_test_df.csv"
+        smote_dataset.to_csv(smote_path, index=False)
 
     print(f"Train/test and future data split complete.")
